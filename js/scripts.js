a8e3eb4462af00dd8782d75bdd59a16562be6eaf
brant porter, Rob mckenzie and Tom Yoder <rob@>
2017-02-23 14:19:02 -0800

    ParseInt roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,28 @@
 var result = (function(number){
   // var digits=[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
   // var roman=["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  newArray = [];
+  number.forEach(function(singleNumber){
+    newArray.push(parseInt(singleNumber));
+  });
+  console.log(newArray);
   var one=["","I","II","III","IV","V","VI","VII","VIII","IX"];
   var ten=["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
   var hundred=["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"];
   var thousand=["","M","MM","MMM"];
   var text2 = "";
-  for(var i=0;i<number.length;i++)
-  if (number.length===1) {
-    text2 = one[number[i]];
+  for(var i=0;i<newArray.length;i++)
+  if (newArray.length===1) {
+    text2 = one[newArray[i]];
     console.log(text2);
-  } else if (number.length===2) {
-    text2 = ten[number[0]] + one[number[i]];
+  } else if (newArray.length===2) {
+    text2 = ten[newArray[0]] + one[newArray[i]];
     console.log(text2);
-  } else if (number.length===3) {
-    text2 = hundred[number[0]] + ten[number[1]] + one[number[i]];
+  } else if (newArray.length===3) {
+    text2 = hundred[newArray[0]] + ten[newArray[1]] + one[newArray[i]];
     console.log(text2);
-  } else if (number.length===4) {
-    text2 = thousand[number[0]] + hundred[number[1]] + ten[number[2]] + one[number[i]];
+  } else if (newArray.length===4) {
+    text2 = thousand[newArray[0]] + hundred[newArray[1]] + ten[newArray[2]] + one[newArray[i]];
   }
   //   if (number=digits[i]) {
   //     return text2 = (roman[i]);
